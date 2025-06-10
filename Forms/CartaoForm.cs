c9daed6d3d2df2faefa67a56c1a39173cf791551
William Chapuis Marciano <william_chapuis@hotmail.com>
2025-06-10 09:37:52 -0300

    teste

--- Forms/CartaoForm.cs
+++ Forms/CartaoForm.cs
@@ -1,4 +1,29 @@
+﻿using BO;
+using Classes;
+using DeskUtilDLL;
+using NTIComponentes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using UtilDLL;
+
 namespace GestaoFinanceira.Forms
 {
+    public partial class CartaoForm : Form
+    {
+        public CartaoForm()
+        {
+            InitializeComponent();
+        }
 
+        private void CartaoForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
 }
